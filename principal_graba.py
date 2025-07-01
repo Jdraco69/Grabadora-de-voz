2dea3e09e3823e9fb266597ed22d59e105a8985a
JesusCD <jesusdanielgraterols@gmail.com>
2025-07-01 18:30:55 -0400

    en esteste tercer commit se le añadio una opcion de menu , donde se puede customisar el tiempo de duracion del audio , todo mediante la interfas

--- principal_graba.py
+++ principal_graba.py
@@ -1,11 +1,11 @@
 import sounddevice as sou
 from scipy.io.wavfile import write
+def audio(segundos):
+ fs=44100 
+ seconds=segundos
+ print("recording...")
 
-fs=44100 
-seconds=10
-print("recording...")
+ record_voice=sou.rec(int(seconds * fs),samplerate=fs,channels=2)
+ sou.wait()
 
-record_voice=sou.rec(int(seconds * fs),samplerate=fs,channels=2)
-sou.wait()
-
-write("out.wav",fs,record_voice)
+ write("out.wav",fs,record_voice)
