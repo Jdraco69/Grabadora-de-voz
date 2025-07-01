ba7f2f120e75def0b876152465bd89438992fc95
JesusCD <jesusdanielgraterols@gmail.com>
2025-07-01 10:52:48 -0400

    este comit posee el progrma basico , que permite grabar el audio

--- principal_graba.py
+++ principal_graba.py
@@ -1,1 +1,11 @@
-import sounddevice
+import sounddevice as sou
+from scipy.io.wavfile import write
+
+fs=44100 
+seconds=10
+print("recording...")
+
+record_voice=sou.rec(int(seconds * fs),samplerate=fs,channels=2)
+sou.wait()
+
+write("out.wav",fs,record_voice)
